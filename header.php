f375a5484348dcd9e5bae7f596164992b8510098
Raik Ilves <raik.ilves@gmail.com>
2014-08-23 16:14:50 +0300

    Add favicon and icons for apple and ms devices

--- header.php
+++ header.php
@@ -4,6 +4,23 @@
 	<meta charset="UTF-8">
 	<title><?php wp_title(); ?></title>
 
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicons/favicon.ico" type="image/x-icon" />
+	<!-- Apple Touch Icons -->
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/favicons/apple-touch-icon-152x152.png" />
+	<!-- Windows 8 Tile Icons -->
+	<meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/img/favicons/smalltile.png" />
+	<meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/img/favicons/mediumtile.png" />
+	<meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/img/favicons/widetile.png" />
+	<meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/img/favicons/largetile.png" />
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
